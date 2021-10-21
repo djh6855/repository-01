62423313233618ac52cc41116b803e9cd1d2623c
djh6855 <2932562317@qq.com>
2021-10-21 14:57:00 +0800

    第二次添加

--- Hutool/src/main/java/com/djh/HutoolTest.java
+++ Hutool/src/main/java/com/djh/HutoolTest.java
@@ -2,11 +2,15 @@ package com.djh;
 
 import cn.hutool.core.*;
 import cn.hutool.core.convert.Convert;
+import cn.hutool.core.lang.Console;
+import cn.hutool.log.dialect.console.ConsoleLog;
 
 public class HutoolTest {
     public static void main(String[] args) {
         String[] arr = {"1", "2", "3"};
         Integer[] ints = Convert.toIntArray(arr);
-        System.out.println(ints);
+        Console.log(ints);
+        Console.log("---------");
+        Console.log("----第二次添加-----");
     }
 }
